a8326b2030a627f1122d7c81ee9cac32a877650a
paleo9 <s5n.wardill@gmail.com>
2021-11-21 22:51:07 +0000

    initial commit

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,6 @@
+-- this file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function()
+	-- Packer can manage itself
+	use 'wbtomason/packer.nvim'
+end)
